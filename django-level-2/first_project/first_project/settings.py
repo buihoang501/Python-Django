ba334ae925367131c21e8d30c16abd62c1a45e03
Hoang Bui <minhhoangbuingo501@gmail.com>
2023-10-16 01:21:51 +0700

    creating models

--- django-level-2/first_project/first_project/settings.py
+++ django-level-2/first_project/first_project/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR =  os.path.join(BASE_DIR,"templates")
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "first_app"
 ]
 
 MIDDLEWARE = [

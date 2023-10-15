6d703002fe4142ea0593534ad00f21b912825434
Hoang Bui <minhhoangbuingo501@gmail.com>
2023-10-15 19:43:23 +0700

    templates

--- django-level-1/first_project/first_app/views.py
+++ django-level-1/first_project/first_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!!")
+    my_dict = {'my_variable':'Hello developer!'}
+    return render(request,'first_app/index.html',context=my_dict)

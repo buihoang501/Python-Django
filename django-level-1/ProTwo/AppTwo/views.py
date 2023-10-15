c4efad5b538fb3b1f3e458d9109f3947b60a8de7
Hoang Bui <minhhoangbuingo501@gmail.com>
2023-10-15 20:18:00 +0700

    challenge templates

--- django-level-1/ProTwo/AppTwo/views.py
+++ django-level-1/ProTwo/AppTwo/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<em>My Second App</em>')
+
+def help(request):
+    dict_value ={'helpmessage':'Help Page!'}
+    return render(request, 'help/index.html',context= dict_value)
